efd4fff68d0201d5a1636a65625a787242ecf73f
JakeSmith1 <jakesmith5757@gmail.com>
2017-02-14 08:14:20 -0500

    form submission listener and data retreival

--- client/js/todoForm.js
+++ client/js/todoForm.js
@@ -1,11 +1,18 @@
-window.TodoForm = (function() {
-  var title; //... elements
-  var forEach;
+window.TodoForm = (function($) {
+  var forEach = function(items, cb) {return Array.prototype.forEach.call(items, cb);},
+   $form;
+   
   function init() {
-    forEach = function(items, cb) {return Array.prototype.forEach.call(items, cb);}
     forEach(document.querySelectorAll('input[name$=-date]'), function(input){input.value = new Date().toDateInputValue();})
+
+    $form = $('.ui .form');
+
+    $form.submit(function(e){
+      e.preventDefault();
+      console.log($form.form('get values'));
+    })
   }
   return {
     init
   }
-})()
+})(jQuery)
