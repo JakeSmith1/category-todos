681f26a0a691cf89ad2528401da2a2b4aeac61f9
JakeSmith1 <jakesmith5757@gmail.com>
2017-02-22 08:19:55 -0500

    cors middleware added

--- server/middleware/appMiddlware.js
+++ server/middleware/appMiddlware.js
@@ -1,8 +1,10 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 // setup global middleware here
 
 module.exports = function(app) {
+  app.use(cors());
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
