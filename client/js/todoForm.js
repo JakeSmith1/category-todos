b99cbcb48ee3ee7a6848d6853cd5699c0c24d78c
JakeSmith1 <jakesmith5757@gmail.com>
2017-02-22 08:18:41 -0500

    ajax post request with form data created

--- client/js/todoForm.js
+++ client/js/todoForm.js
@@ -10,21 +10,16 @@ window.TodoForm = (function($) {
     $form.submit(function(e){
       e.preventDefault();
       var todo = JSON.stringify($form.form('get values'));
-      var url = 'http://localhost:3005/api/todos';
-      console.log(todo, 'values line 14 todoForm.js');
-      //---------------- add check on values ---------------
-      // $.post(url, values)
-      // .then()
-      // .catch();
-      // $.ajax({
-      //   url: url,
-      //   type: "POST",
-      //   data: todo,
-      //   contentType: "application/json"
-      //   // ,
-      //   // complete: callback
-      // })
-      // .then().catch();
+      var url = 'http://localhost:3004/api/categories/';
+      /*---------------- add check on values ---------------*/
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: todo,
+        contentType: "application/json"
+      })
+      .then(function(data){console.log('success', data);})
+      .catch(function(err){console.log('error', err);});
 
     })
   }
